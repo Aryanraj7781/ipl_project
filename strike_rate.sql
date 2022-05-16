f8ec7b99527e278e80d437ebd2b1684b7261a1f4
Aryanraj7781 <105636548+Aryanraj7781@users.noreply.github.com>
2022-05-16 15:44:34 +0530

    Update strike_rate.sql
    PROFILE OF BATSMAN

--- strike_rate.sql
+++ strike_rate.sql
@@ -2,13 +2,18 @@ select count(distinct(id)) as match_played,count(id) as ball_faced ,batsman ,mat
 where  match_year = 2014 
 group by batsman
 order by strike_rate desc , match_played asc;
+
 create table ipl.ipl_run as select count(distinct(id)) as match_played , sum(total_runs) as run, count(id) as ball,((sum(total_runs)/count(id))*100) as strike_rate,batsman ,batting_team from ipl.deliveries_v06 
 group by batsman order by run desc;
 select * from ipl.ipl_run; 
+
 alter table ipl.ipl_run drop column batting_team;
 select batsman,count(ball_result) as boundary from ipl.deliveries_v03 group by batsman order by boundary ;
+
 create table ipl.four select count(ball_result) as four ,batsman from ipl.deliveries_v03 where total_runs = 4 group by batsman;
+
 create table ipl.six select count(ball_result) as six ,batsman from ipl.deliveries_v03 where total_runs = 6 group by batsman;
+
 create table ipl.player as 
   select a.*,b.six 
   from ipl.play as a
